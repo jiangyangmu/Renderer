43e534009ae4b227a2fecbe701b019d225893cc6
Yangmu Jiang <celsius.j@gmail.com>
2021-01-10 03:49:28 -0800

    IUnknown.

--- Source/Resource.h
+++ Source/Resource.h
@@ -8,7 +8,39 @@ namespace Graphics
 {
 namespace v2
 {
+	// ---------------------------------------------------------------
+	// Handle
+	// ---------------------------------------------------------------
+
 	struct Handle { void * pImpl; void * pParam; };
+
+	// ---------------------------------------------------------------
+	// IUnknown
+	// ---------------------------------------------------------------
+
+	// IID: Interface IDentifier
+#define _INTERFACE_DEFINE_IID(iid) \
+        public: static Integer		__IINTERFACE_IID() { return (iid); }
+#define _INTERFACE_IID(interface_) \
+	(interface_::__IINTERFACE_IID())
+#define _INTERFACE_TEST_IID(interface_, iid) \
+	((interface_::__IINTERFACE_IID()) == (iid))
+
+	class IUnknown
+	{
+	public:
+		virtual			~IUnknown() = default;
+
+		virtual bool		QueryInterface(Integer iid, void ** ppvObject)
+		{
+			return false;
+		}
+		template <typename T>
+		bool			QueryInterface(T ** ppObject)
+		{
+			return QueryInterface(_INTERFACE_IID(T), (void **)ppObject);
+		}
+	};
 
 	// ---------------------------------------------------------------
 	// Buffers
